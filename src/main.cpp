6763d80a4492a699a0abf48ce1d889f7242b6f5b
RashmikaDushan <kbrashmikadushan@gmail.com>
2024-03-14 22:39:04 +0530

    us

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,40 @@
 #include <avr/interrupt.h>
 #include <Arduino.h>
 
-uint16_t counter;
+volatile uint16_t counter = 0;
 
 ISR(TIMER1_OVF_vect)
 {
-  TCNT1 = 49535; // Timer Preloading
+  TCNT1 = 65519; // Timer Preloading
   // Handle The 1ms Timer Interrupt
   //...
   counter++;
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
-void delay_ms(uint16_t ms){
+// void delay_ms(uint16_t ms)
+// {
+//   counter = 0;
+//   while (counter < ms)
+//   {
+//     // Wait
+//   }
+// }
+
+void delay_us(uint16_t us)
+{
   counter = 0;
-  while(counter<ms){
-    // wait
+  while (counter < us)
+  {
+    // Wait
   }
 }
- 
+
 void setup()
 {
   TCCR1A = 0;           // Init Timer1
   TCCR1B = 0;           // Init Timer1
   TCCR1B |= 0b00000001;  // No prescaller
-  TCNT1 = 49535;        // Timer Preloading
+  TCNT1 = 65519;        // Timer Preloading
   TIMSK1 |= 0b00000001;  // Enable Timer Overflow Interrupt
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
@@ -33,7 +43,9 @@ void setup()
 void loop()
 {
   Serial.println("Start");
-  delay_ms(2000);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay_us(1000);
   Serial.println("end");
-  delay_ms(2000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay_us(1000);
 }
